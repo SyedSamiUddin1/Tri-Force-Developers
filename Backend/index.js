9daed103ab3dfd9ce2d5096c90da55b039fc220c
Rahim Gangji <rahimgangji428@gmail.com>
2025-06-15 21:22:06 +0500

    fixes

--- Backend/index.js
+++ Backend/index.js
@@ -9,10 +9,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
+    cors({
+        origin: "https://tri-force-developers.vercel.app",
+        credentials: true,
+    })
 );
 connectDB();
 app.use("/api/auth", userRouter);
